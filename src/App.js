a742b9df4806f6cacfa0dee760b3b474862b5fe9
Kaleb-Rupe <57547999+Kaleb-Rupe@users.noreply.github.com>
2023-04-07 01:48:44 -0400

    App component is wrapped in BrowserRouter in the index.js module. Homepage links to Order page view nav link with the id of order-pizza.

--- src/App.js
+++ src/App.js
@@ -1,11 +1,27 @@
 import React from "react";
+import { Routes, Route, Link } from "react-router-dom";
+
+function Home(props) {
+  return <h2>Home</h2>;
+}
+
+function Order(props) {
+  return <h2>Order</h2>;
+}
 
 const App = () => {
   return (
-    <>
-      <h1>Lambda Eats</h1>
-      <p>You can remove this code and create your own header</p>
-    </>
+    <div>
+      <h1>BloomTech Eats</h1>
+      <nav>
+        <Link to="/">Home</Link>&nbsp;
+        <Link to="order">Order</Link>
+      </nav>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="order" element={<Order />} />
+      </Routes>
+    </div>
   );
 };
 export default App;
